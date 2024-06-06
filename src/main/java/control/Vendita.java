8173ed210b4f8fd4f4762f84bb617968fdbaa489
Stella Tavolo <Stella Tavolo@Pc>
2024-06-06 18:55:23 +0200

    id-4 resolved

--- src/main/java/control/Vendita.java
+++ src/main/java/control/Vendita.java
@@ -5,17 +5,15 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.http.fileupload.FileItem;
-import org.apache.tomcat.util.http.fileupload.RequestContext;
 import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
 import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
 import org.apache.tomcat.util.http.fileupload.servlet.ServletRequestContext;
@@ -28,8 +26,8 @@ import model.ProductModel;
  */
 @WebServlet("/Vendita")
 public class Vendita extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -38,79 +36,88 @@ public class Vendita extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ProductBean product = new ProductBean();
-		product.setEmail((String) request.getSession().getAttribute("email"));
-		
-		 String UPLOAD_DIRECTORY = request.getServletContext().getRealPath("/")+"img/productIMG/";
-		    //process only if its multipart content
-		    if(ServletFileUpload.isMultipartContent(request)) {
-		        try {
-		            List<FileItem> multiparts = new ServletFileUpload(
-		                                     new DiskFileItemFactory()).parseRequest(new ServletRequestContext(request));
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // This servlet is designed to handle only POST requests, so all GET requests
+        // are redirected
+        response.sendRedirect(request.getContextPath() + "/index.jsp");
+    }
 
-		            for(FileItem item : multiparts){
-		                if(!item.isFormField()){
-		                    String name = new File(item.getName()).getName();
-		                    item.write( new File(UPLOAD_DIRECTORY + File.separator + name));
-		                    product.setImmagine(name);
-		                }
-		                else {
-		                	if (item.getFieldName().compareTo("nome") == 0) {
-		                		product.setNome(item.getString());
-		                	}
-		                	else if (item.getFieldName().compareTo("prezzo") == 0) {
-		                		product.setPrezzo(Double.parseDouble(item.getString()));
-		                	}
-		                	else if (item.getFieldName().compareTo("spedizione") == 0) {
-		                		product.setSpedizione(Double.parseDouble(item.getString()));
-		                	}
-		                	else if (item.getFieldName().compareTo("tipologia") == 0) {
-		                		product.setTipologia(item.getString());
-		                	}
-							else if (item.getFieldName().compareTo("tag") == 0) {
-								product.setTag(item.getString());
-							}
-							else if (item.getFieldName().compareTo("descrizione") == 0) {
-		                		product.setDescrizione(item.getString());
-		                	}
-		                }
-		            }
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ProductBean product = new ProductBean();
+        product.setEmail((String) request.getSession().getAttribute("email"));
 
-		           //File uploaded successfully
-		           request.setAttribute("message", "File Uploaded Successfully");
-		           
-		        } catch (Exception ex) {
-		           
-		        }          
+        String UPLOAD_DIRECTORY = request.getServletContext().getRealPath("/") + "img/productIMG/";
 
-		    }
-		    else{
-		        request.setAttribute("message",
-		                             "Sorry this Servlet only handles file upload request");
-		       
-		    }
-		    ProductModel model = new ProductModel();
-		    try {
-				model.doSave(product);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		    request.getSession().setAttribute("refreshProduct", true);
-		    request.getRequestDispatcher("/index.jsp").forward(request, response);
-		}
-		    
+        if (ServletFileUpload.isMultipartContent(request)) {
+            try {
+                List<FileItem> multiparts = new ServletFileUpload(new DiskFileItemFactory())
+                        .parseRequest(new ServletRequestContext(request));
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+                for (FileItem item : multiparts) {
+                    if (!item.isFormField()) {
+                        String name = new File(item.getName()).getName();
+                        item.write(new File(UPLOAD_DIRECTORY + File.separator + name));
+                        product.setImmagine(name);
+                    } else {
+                        String fieldName = item.getFieldName();
+                        String fieldValue = item.getString("UTF-8");
+                        // Sanitize input
+                        fieldValue = sanitizeInput(fieldValue);
+                        if (fieldName.equals("nome")) {
+                            product.setNome(fieldValue);
+                        } else if (fieldName.equals("prezzo")) {
+                            product.setPrezzo(Double.parseDouble(fieldValue));
+                        } else if (fieldName.equals("spedizione")) {
+                            product.setSpedizione(Double.parseDouble(fieldValue));
+                        } else if (fieldName.equals("tipologia")) {
+                            product.setTipologia(fieldValue);
+                        } else if (fieldName.equals("tag")) {
+                            product.setTag(fieldValue);
+                        } else if (fieldName.equals("descrizione")) {
+                            product.setDescrizione(fieldValue);
+                        }
+                    }
+                }
 
+                request.setAttribute("message", "File Uploaded Successfully");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                request.setAttribute("message", "File Upload Failed due to " + ex);
+            }
+        } else {
+            request.setAttribute("message", "Sorry this Servlet only handles file upload request");
+        }
+
+        ProductModel model = new ProductModel();
+        try {
+            model.doSave(product);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        request.getSession().setAttribute("refreshProduct", true);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
+
+    private String sanitizeInput(String input) {
+        // Sanitize input to prevent XSS attacks
+        String cleanedInput = input.replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+        cleanedInput = stripScriptTags(cleanedInput);
+        return cleanedInput;
+    }
+
+    private String stripScriptTags(String input) {
+        // Strip <script> tags and their content
+        return input.replaceAll("(?i)<script.*?>.*?</script.*?>", "");
+    }
 }
